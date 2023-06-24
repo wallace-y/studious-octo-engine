29f06e6038c844117ed7801f1d814bef3ac8d9c3
wallace-y <cameronwfinney@gmail.com>
2023-06-24 17:30:36 +0100

    adding basic outline to notes app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,178 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
+import { Container, TextField, Typography, Button, Box } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Typography variant="h1">Your to do list</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <TextField id="filled-basic" variant="filled" placeholder="To Do" fullWidth/>
+            <Grid container alignItems="center" sx={{ marginTop: "16px" }}>
+              <Grid item xs={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Add note"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Button style={{ minHeight: "55px" }} variant="outlined" fullWidth>
+                  +
+                </Button>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              id="filled-basic"
+              variant="filled"
+              placeholder="In Progress"
+              fullWidth
+            />
+            <Grid container alignItems="center" sx={{ marginTop: "16px" }}>
+              <Grid item xs={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Add note"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Button style={{ minHeight: "55px" }} variant="outlined" fullWidth>
+                  +
+                </Button>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+          </Grid>
+          <Grid item xs={4}>
+            <TextField id="filled-basic" variant="filled" placeholder="Done" fullWidth/>
+            <Grid container sx={{ marginTop: "16px" }}>
+              <Grid item xs={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Add note"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Button style={{ minHeight: "55px" }} variant="outlined" fullWidth>
+                  +
+                </Button>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+            <Box
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                padding: "8px",
+                marginTop: "8px",
+              }}
+            >
+              <Typography variant="body1">This is a note</Typography>
+            </Box>{" "}
+          </Grid>
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
